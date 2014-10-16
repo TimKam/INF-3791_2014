324dc2b2f291b01d17042b36de52eb64d091d833
TimKam <timotheus.kampik@gmail.com>
2014-10-16 14:22:42 +0200

    the request against the weather API is now made based on the device's GPS location. Organized imports

--- AndroidApplication/src/com/example/copdmgmtapp/MainActivity.java
+++ AndroidApplication/src/com/example/copdmgmtapp/MainActivity.java
@@ -1,35 +1,26 @@
 package com.example.copdmgmtapp;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.URLEncoder;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import android.support.v7.app.ActionBarActivity;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import java.net.*;
-import android.view.*;
-import android.graphics.*;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -42,10 +33,24 @@ public class MainActivity extends ActionBarActivity {
 			@Override
 		    public void run() 
 		    {
+				//get current location
+				LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+				String locationProvider = LocationManager.GPS_PROVIDER;
+				Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+				double currentLatitude = lastKnownLocation.getLatitude();
+				Log.d("current latitude", String.valueOf(currentLatitude));
+				double currentLongitude = lastKnownLocation.getLongitude();
+				Log.d("current longitude", String.valueOf(currentLongitude));
+				
+				
 		    	//API calls example (http://stackoverflow.com/questions/4457492/how-do-i-use-the-simple-http-client-in-android):
 				HttpClient httpclient = new DefaultHttpClient();
-			    // Prepare a request object
-			    HttpGet httpget = new HttpGet("http://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10;lon=9.58;msl=70"); 
+			    // Prepare a request object against weather api with current Latitude/longitude
+			    HttpGet httpget = new HttpGet("http://api.met.no/weatherapi/locationforecast/1.9/?lat=" +
+			    currentLatitude +
+			    ";lon=" +
+			    currentLongitude +
+			    ";msl=70"); 
 
 			    // Execute the request
 			    HttpResponse response;
